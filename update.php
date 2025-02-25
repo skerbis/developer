adfcad4dd668b7176e8419ce7974593ad436e5a2
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-25 11:58:36 +0100

    Update update.php

--- update.php
+++ update.php
@@ -21,3 +21,23 @@ if (rex_string::versionCompare($this->getVersion(), '3.6.0', '<')) {
 if (rex_string::versionCompare($this->getVersion(), '3.6.0-beta2', '<')) {
     $this->setConfig('dir_suffix', false);
 }
+
+if (rex_string::versionCompare($this->getVersion(), '3.10.0', '<')) {
+    if (!$this->hasConfig('versioning')) {
+        $this->setConfig('versioning', true);
+    }
+    if (!$this->hasConfig('max_versions')) {
+        $this->setConfig('max_versions', 10);
+    }
+    
+    // Create the versions directory if it doesn't exist
+    $versionDir = rex_path::addonData('developer', rex_developer_synchronizer_version::VERSION_DIR);
+    if (!rex_dir::create($versionDir)) {
+        rex_logger::logError(
+            E_WARNING,
+            'Failed to create versions directory: ' . $versionDir,
+            __FILE__,
+            __LINE__
+        );
+    }
+}
